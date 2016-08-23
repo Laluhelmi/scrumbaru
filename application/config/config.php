61dc5aa0c8e8e3d36af0817f3de4885a3dc67419
root <pratamasetya99@gmail.com>
2016-08-24 04:07:33 +0700

    index

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://apiscrum.alfatech.id/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
